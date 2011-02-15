0be2ab5028dd4b468b82f68e526d71ed544cfd82
Mircea Markus <Mircea.Markus@gmail.com>
2011-02-15 16:39:42 +0000

    specify jdk

--- framework/src/main/resources/environment.sh
+++ framework/src/main/resources/environment.sh
@@ -1,12 +1,11 @@
 #!/bin/sh
-###### This script is designed to be called from other scripts, to set environment variables including the bind
-###### for cache products, as well as any JVM options.
 
-### Set your bind address for the tests to use. Could be an IP, host name or a reference to an environment variable.
+
+export JAVA_HOME=/qa/tools/opt/jdk1.6.0_20
+PATH=$JAVA_HOME/bin:$PATH
+
 BIND_ADDRESS=`/sbin/ifconfig eth1 | grep -v inet6 | grep inet | cut -d':' -f2 | cut -d' ' -f1`
 JG_FLAGS="-Dresolve.dns=false -Djgroups.timer.num_threads=4"
 JVM_OPTS="-server -Xmx1024M -Xms1024M"
 JVM_OPTS="$JVM_OPTS $JG_FLAGS"
-JPROFILER_HOME=${HOME}/jprofiler6
-JPROFILER_CFG_ID=103
 
